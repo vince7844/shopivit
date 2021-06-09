d297afd04d4bff7a7c6b62418bc2b712bc3f46ab
vince7844 <vincent.lasserre@efrei.net>
2021-06-09 09:00:52 +0300

    moved the directory, shop data into its own reducer, created respective selector and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import { connect } from 'react-redux';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import { createStructuredSelector } from 'reselect'
-import { selectShopCollections } from '../../redux/shop/shop.selector';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    {
-      collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))
-    }
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,21 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
   [selectShop],
   shop => shop.collections 
+)
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectShopCollection = memoize((collectionUrlParam) => 
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+  )
 )
