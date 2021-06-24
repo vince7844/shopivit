13e42bc6e41215b00fe5836b765e3b049b62ff00
vince7844 <vincent.lasserre@efrei.net>
2021-06-24 16:59:07 +0300

    Adding contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
+import ContactPage from "./pages/contact/contact.component";
 
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
@@ -47,6 +48,7 @@ class App extends React.Component {
         <Switch>
          <Route exact path='/' component={HomePage} />
          <Route path='/shop' component={ShopPage} />
+         <Route path='/contact' component={ContactPage} />
          <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage /> }/>
          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './contact.styles.scss';
+
+const Contact = () => (
+  <div className='contact'>
+    <h1 className='title'>Contact</h1>
+    <p className='description'>You can reach the company support at <a className='mail' href="mailto:anome@ctemplar.com">anome@ctemplar.com</a></p>
+  </div>
+)
+
+export default Contact;
