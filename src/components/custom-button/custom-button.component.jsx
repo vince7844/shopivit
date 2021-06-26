5008befd89ce2bda2ba80fcd4e604a7bd571b1cd
vince7844 <vincent.lasserre@efrei.net>
2021-06-26 09:50:02 +0300

    changing collection component styles, with order add to cart button styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { CustomButtonContainer } from "./custom-button.styles";
+import React from 'react';
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props} >
+import './custom-button.styles.scss';
+
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
     {children}
-  </CustomButtonContainer>
-)
+  </button>
+);
 
 export default CustomButton;
